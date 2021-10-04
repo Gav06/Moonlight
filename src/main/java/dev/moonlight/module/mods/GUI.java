4c2b83f9b5ec408e839a430c4ba2d5f847200cc1
vikkn <vikaslagerweij@gmail.com>
2021-10-04 15:10:19 +0200

    gui settings

--- src/main/java/dev/moonlight/module/mods/GUI.java
+++ src/main/java/dev/moonlight/module/mods/GUI.java
@@ -3,6 +3,7 @@ package dev.moonlight.module.mods;
 import dev.moonlight.module.Module;
 import dev.moonlight.settings.impl.BoolSetting;
 import dev.moonlight.settings.impl.FloatSetting;
+import dev.moonlight.settings.impl.ModeSetting;
 import org.lwjgl.input.Keyboard;
 
 @Module.Info(
@@ -12,14 +13,30 @@ import org.lwjgl.input.Keyboard;
         bind = Keyboard.KEY_GRAVE
 )
 public final class GUI extends Module {
+    public BoolSetting othersParent = new BoolSetting("Others", false, true);
+    public final ModeSetting backgroundMode = new ModeSetting("BackgroundMode", BackgroundMode.None, () -> othersParent.getValue());
 
-    public final BoolSetting background = new BoolSetting("Background", false, false);
-    public final BoolSetting descriptions = new BoolSetting("Descriptions", true, false);
-    public final FloatSetting r = new FloatSetting("R", 255, 0, 255);
-    public final FloatSetting g = new FloatSetting("G", 0, 0, 255);
-    public final FloatSetting b = new FloatSetting("B", 0, 0, 255);
-    public final FloatSetting a = new FloatSetting("A", 255, 0, 255);
-    public final FloatSetting backgroundA = new FloatSetting("WindowA", 255, 0, 255);
+    public enum BackgroundMode {Darken, Blur, None}
+
+    public final BoolSetting descriptions = new BoolSetting("Descriptions", true, false, () -> othersParent.getValue());
+
+    public BoolSetting leftBackgroundparent = new BoolSetting("LeftBackground", false, true);
+    public final FloatSetting lbgr = new FloatSetting("LEFT-BG-R", 0, 0, 255, () -> leftBackgroundparent.getValue());
+    public final FloatSetting lbgg = new FloatSetting("LEFT-BG-G", 0, 0, 255, () -> leftBackgroundparent.getValue());
+    public final FloatSetting lbgb = new FloatSetting("LEFT-BG-B", 0, 0, 255, () -> leftBackgroundparent.getValue());
+    public final FloatSetting lbga = new FloatSetting("LEFT-BG-A", 200, 0, 255, () -> leftBackgroundparent.getValue());
+
+    public BoolSetting rightBackgroundParent = new BoolSetting("RightBackground", false, true);
+    public final FloatSetting rbgr = new FloatSetting("RIGHT-BG-R", 0, 0, 255, () -> rightBackgroundParent.getValue());
+    public final FloatSetting rbgg = new FloatSetting("RIGHT-BG-G", 0, 0, 255, () -> rightBackgroundParent.getValue());
+    public final FloatSetting rbgb = new FloatSetting("RIGHT-BG-B", 0, 0, 255, () -> rightBackgroundParent.getValue());
+    public final FloatSetting rbga = new FloatSetting("RIGHT-BG-A", 200, 0, 255, () -> rightBackgroundParent.getValue());
+
+    public BoolSetting outlineParent = new BoolSetting("OutlineParent", false, true);
+    public final FloatSetting r = new FloatSetting("OUTLINE-R", 255, 0, 255, () -> outlineParent.getValue());
+    public final FloatSetting g = new FloatSetting("OUTLINE-G", 0, 0, 255, () -> outlineParent.getValue());
+    public final FloatSetting b = new FloatSetting("OUTLINE-B", 0, 0, 255, () -> outlineParent.getValue());
+    public final FloatSetting a = new FloatSetting("OUTLINE-A", 255, 0, 255, () -> outlineParent.getValue());
 
     @Override
     protected void onEnable() {
